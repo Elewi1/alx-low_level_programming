850855e563eec7149a6678be0e8a45f3736af4b7
Elewi1 <aohamzat45@student.lautech.edu.ng>
2023-09-11 10:29:22 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,26 +6,23 @@
 
 int main(void)
 {
-	int ones = '0';
-	int tens = '0'0;
+	int first_digit;
+	int second_digit;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (first_digit = 0; first_digit <= 8; first_digit++)
 	{
-		for (ones = '0'; ones <= '9'; ones++);
+		for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
 		{
-			if (!((ones == tens) || (tens > ones)))
+			putchar(first_digit + '0');
+			putchar(second_digit + '0');
+
+			if (first_digit != 8 || second_digit != 9)
 			{
-				puchar(tens);
-				putchar(ones);
-				if (!(ones == '9' && tens == '8'))
-				{
-					putcher(',');
-					putcher(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
