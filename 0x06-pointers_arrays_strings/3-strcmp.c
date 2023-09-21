a9233fd7bcfb3b98168f3060849399a47cbaffef
Elewi1 <aohamzat45@student.lautech.edu.ng>
2023-09-21 15:32:15 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * @s1: input value
  * @s2: input value
  *
- * Return: s1[i] -s2[i]
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] != s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
