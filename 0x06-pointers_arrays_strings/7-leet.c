3092eba4501ae5464b48439bae984753ff0dc580
Elewi1 <aohamzat45@student.lautech.edu.ng>
2023-09-21 17:50:33 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,7 +7,7 @@
 char *leet(char *n)
 {
 	int i, j;
-	char s1[] = "aAeEoOtTIL";
+	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
@@ -16,7 +16,7 @@ char *leet(char *n)
 		{
 			if (n[i] == s1[j])
 			{
-				n[i] == s1[j];
+				n[i] = s2[j];
 			}
 		}
 	}
